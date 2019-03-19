c5413a883f71582a12bde7d8b1bea3895d616f6a
Rose Stough <psithurise@programmer.net>
2019-03-19 22:34:15 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,8 +4,7 @@ var playlist = {
 }
 
 function updatePlaylist(obj, key, val) {
-  obj.key = val
-  return obj
+  return obj.assign({}, obj, {[key]: val})
 }
 
 function removeFromPlaylist(obj, key) {
